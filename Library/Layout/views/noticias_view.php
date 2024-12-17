4339b9afc9df94dc5935a7d357966b52b1263698
AlanAvila97 <fidelavila21@gmail.com>
2024-12-17 12:00:13 -0600

    Creacion de vistas de las secciones

---
+++ Library/Layout/views/noticias_view.php
@@ -0,0 +1,34 @@
+<section class="flex-news pt-md-70">
+        <article class="news">
+            <h2 class="title">News</h2>
+            <div class="article-imagen">
+                <img src="https://cherry-brightspot.s3.amazonaws.com/media/2012/03/22/oncetv-mexico-cuartoscuro.jpg" alt="Descripción de la imagen">
+                <div class="text">
+                    <p> Senado aprueba dictamen que prohibe vapeadores; pasa a congresos estatales</p>
+                </div>
+            </div>
+        </article>
+    </section>
+    <section class="grid-newss-notas">
+        <div class="container-notes">
+            <article class="item-nota">
+                <img src="https://play-lh.googleusercontent.com/5HtLz18czU8gjDXRtdfgmMPjx3BnxF0FIbqq7HjoVb7n73T5FoPUwGMJFYotCBMXtw" alt="Imagen 5">
+                <div class="text-overlay-nota">
+                    <h3>TITLE NOTE</h3>
+                </div>
+            </article>
+            <article class="item-nota middle ">
+                <img src="https://play-lh.googleusercontent.com/5HtLz18czU8gjDXRtdfgmMPjx3BnxF0FIbqq7HjoVb7n73T5FoPUwGMJFYotCBMXtw" alt="Imagen 5">
+                <div class="contenido">
+                    <h3 class="title">TíITLE NEW</h3>
+                    <p class="parrafo">Lorem ipsum dolor sit amet consectetur adipiscing elit sapien nulla cum id, dictum justo mollis taciti netus vivamus pellentesque blandit libero laoreet.</p>
+                </div>
+            </article>
+            <article class="item-nota">
+                <img src="https://play-lh.googleusercontent.com/5HtLz18czU8gjDXRtdfgmMPjx3BnxF0FIbqq7HjoVb7n73T5FoPUwGMJFYotCBMXtw" alt="Imagen 5">
+                <div class="text-overlay-nota">
+                    <h3>TITLE NEW</h3>
+                </div>
+            </article>
+        </div>
+    </section>

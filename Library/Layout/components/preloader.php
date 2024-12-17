15dfe53fde80545d59362cfa7d8d391ccffd3e1b
AlanAvila97 <fidelavila21@gmail.com>
2024-12-17 12:01:07 -0600

    Creacion de componentes del sitio

---
+++ Library/Layout/components/preloader.php
@@ -0,0 +1,7 @@
+<!-- Preloader -->
+<article class="preloader">
+    <figure>
+        <img class="img-fluid img-loader pulse-infinite" src="https://canalonce.mx/REST/data/images/logomxplus.png" alt="Logo Once" width="250" height="81" loading="lazy">
+    </figure>
+</article>
+<!-- End Preloader -->

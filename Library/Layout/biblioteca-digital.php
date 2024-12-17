8ad4ea02a70f6139d4b7a24b461061a0bf67fa34
AlanAvila97 <fidelavila21@gmail.com>
2024-12-17 12:02:14 -0600

    creacion de layouts faltantes

--- Library/Layout/biblioteca-digital.php
+++ Library/Layout/biblioteca-digital.php
@@ -2,7 +2,7 @@
 <html lang="es-MX">
 <head>
     <meta charset="UTF-8">
-    <title>Acordeones || <?php echo $titleName; ?></title>
+    <title>Bliblioteca Digital || <?php echo $titleName; ?></title>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
@@ -17,16 +17,22 @@
     <meta name="twitter:site" content="">
     <!-- Elements Header -->
 	<?php require_once("./Library/Layout/components/header_elements.php");?>
-<body class="acordeones">
+    <!-- End Elements Header -->
+    <!-- Buscador -->
+    <link rel="stylesheet" href="./assets/css/buscador.css" media="none" onload="if(media!='all')media='all'"/>
+    <noscript><link rel="stylesheet" href="./assets/css/buscador.css"></noscript>
+<body class="biblioteca">
     <!-- Preloader -->
 	<?php require_once("./Library/Layout/components/preloader.php");?>
     <!-- End Preloader -->
     <!-- Navbar -->
 	<?php require_once("./Library/Layout/components/nabvar.php");?>
     <!-- End Navbar -->
-    <!-- View -->
-	<?php require_once("./Library/Layout/views/acordeones_view.php");?>
-    <!-- End View -->
+    <main id="wrapper">
+        <!-- View -->
+        <?php require_once("./Library/Layout/views/biblioteca_digital_view.php");?>
+        <!-- End View -->
+    </main>
     <!-- Footer  -->
 	<?php require_once("./Library/Layout/components/footer.php");?>
     <!-- End Footer  -->     

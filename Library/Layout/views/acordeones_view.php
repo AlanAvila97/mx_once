6df09c070d37c972e37f93622d2a5527cb718041
AlanAvila97 <fidelavila21@gmail.com>
2024-12-17 18:34:00 -0600

    Vista de acordeones

---
+++ Library/Layout/views/acordeones_view.php
@@ -0,0 +1,17 @@
+<section class="section-acordeones pt-md-70">
+        <h1>ACORDEONES</h1>
+        <div class="container-acordeones">
+            <div class="image-item">
+                <a href=""><img src="https://d22jn00zjrk7sl.cloudfront.net/wp-content/uploads/2024/12/logOnce.jpg" alt="Imagen 1"></a>
+                <h3>Title img</h3>
+            </div>
+            <div class="image-item">
+                <a href=""><img src="https://d22jn00zjrk7sl.cloudfront.net/wp-content/uploads/2024/12/logOnce.jpg" alt="Imagen 2"></a>
+                <h3>Title img</h3>
+            </div>
+            <div class="image-item">
+                <a href=""><img src="https://d22jn00zjrk7sl.cloudfront.net/wp-content/uploads/2024/12/logOnce.jpg" alt="Imagen 3"></a>
+                <h3>Title img</h3>
+            </div>
+        </div>
+    </section>

ecccb43ccfd9dfd07a5cfcb8499db51af2cc9369
AlanAvila97 <fidelavila21@gmail.com>
2024-12-23 16:55:18 -0600

    pantalla interna de reproductor tv

---
+++ Library/Layout/reproductor_tv.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <title>Tv En vivo || <?php echo $titleName; ?></title>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta property="og:title" content="">
+    <meta property="og:description" content="">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <meta property="article:publisher" content="">
+    <meta property="og:image" content="">
+    <meta name="twitter:site" content="">
+    <!-- Elements Header -->
+	<?php require_once("./Library/Layout/components/header_elements.php");?>
+    <!-- Video JS -->  
+    <link rel="stylesheet" href="./assets/css/videojs/video-js.min.css" media="none" onload="if(media!='all')media='all'"/>
+    <noscript><link rel="stylesheet" href="./assets/css/videojs/video-js.min.css"></noscript>
+    <link rel="stylesheet" href="./assets/css/videojs/videojs-http-source-selector.css" media="none" onload="if(media!='all')media='all'"/>
+    <noscript><link rel="stylesheet" href="./assets/css/videojs/videojs-http-source-selector.css"></noscript>
+    <!--  -->
+    <link rel="stylesheet" href="./assets/css/en-vivo.css" media="none" onload="if(media!='all')media='all'"/>
+    <noscript><link rel="stylesheet" href="./assets/css/en-vivo.css"></noscript>
+<body class="en-vivo bg-envivo-digital">
+    <!-- Preloader -->
+	<?php require_once("./Library/Layout/components/preloader.php");?>
+    <!-- End Preloader -->
+    <!-- Navbar -->
+	<?php require_once("./Library/Layout/components/nabvar.php");?>
+    <!-- End Navbar -->
+    <main id="wrapper">
+        <!-- View -->
+        <?php require_once("./Library/Layout/views/reproductores/interior_tv_view.php"); ?>
+        <!-- End View -->
+    </main>
+    <!-- Footer  -->
+	<?php require_once("./Library/Layout/components/footer.php");?>
+    <!-- End Footer  -->     
+    <!--  Footer Elements  -->
+    <?php require_once("./Library/Layout/components/footer_elements.php");?>
+    <!-- End Footer Elements-->
+    <!-- Moment JS -->
+    <script defer src="./assets/js/momentjs/moment.js"></script>
+	<script defer src="./assets/js/momentjs/moment-with-locales.js"></script>
+	<script defer src="./assets/js/momentjs/moment-timezone-with-data.js"></script>
+    <!-- Video Js -->
+    <script defer src="./assets/js/videojs/video.min.js"></script>
+    <script defer src="./assets/js/videojs/Youtube.min.js"></script>
+    <script defer src='./assets/js/videojs/videojs-http-source-selector.js'></script>
+    <script defer src='./assets/js/videojs/videojs-contrib-quality-levels.min.js'></script>
+    <script defer src="./assets/js/videojs/videojs-landscape-fullscreen.min.js"></script>
+    <script defer src="./assets/js/videojs/videojs-player-analytics.js"></script>
+    <script defer src="./assets/js/videojs/videojs-vimeo.umd.js"></script>
+    <script defer src="./assets/js/videojs/videojs.hotkeys.min.js"></script>
+    <!--  -->
+    <script defer src="./assets/js/en-vivo.js"></script>
+</body>
---
+++ Library/Layout/views/reproductores/interior_tv_view.php
@@ -0,0 +1,36 @@
+<section class="section-reproductor-live pt-md-70">
+    <div class="container-reproductor-live">
+        <div class="title-reproductor-live logo-once3">
+            <picture>
+                <source class="lazy img-fluid" srcset="./assets/images/MX+Logotipo_curvas_color.svg" type="image/webp" alt="Logo Señal Digital once.3" loading="lazy">
+                <source class="lazy img-fluid" srcset="./assets/images/MX+Logotipo_curvas_color.svg" type="image/png" alt="Logo Señal Digital once.3" loading="lazy">
+                <img class="lazy img-fluid img-digital" src="./assets/images/MX+Logotipo_curvas_color.svg" alt="Logo Señal Digital once.3" loading="lazy" title="Logo Señal Digital once.3">
+            </picture>
+        </div>
+        <div class="content-reproductor-live">
+            <div class="reproductor-live center-items">                        
+                <video id="streamplayer" class="video-js vjs-big-play-centered" controls autoplay preload="auto" playsinline data-setup='{"fluid": true}'>
+                </video>
+            </div>
+        </div>                                   
+        <div class="title-program-live">
+            <h2 class="current-program"></h2>
+            <h2 class="sinopsis-program"></h2>
+            <p class="desc-program"></p>
+        </div> 
+    </div>
+</section>
+<section class="section-programation-live">
+    <div class="container-programation-live ">
+        <div class="info-pgm">
+            <div class="text-informacion">
+                <h2>
+                    Para más información
+                    <a href="./horarios" rel="Link para horarios">
+                        consulta nuestros horarios aquí
+                    </a>
+                </h2>
+            </div>
+        </div>
+    </div>
+</section> 

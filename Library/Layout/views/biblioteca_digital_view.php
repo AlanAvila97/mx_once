9a81088721e3b5cfbd867bd2ad9afa3be66fc999
AlanAvila97 <fidelavila21@gmail.com>
2024-12-23 11:21:51 -0600

    funcioamiento y vistas de bliblioteca digital

--- Library/Layout/views/biblioteca_digital_view.php
+++ Library/Layout/views/biblioteca_digital_view.php
@@ -27,59 +27,6 @@
             <h2>Resultados de la busqueda:</h2>
         </div>
         <div class="results d-grid">
-            <!-- <a href="#" class="item-result d-flex">
-                <div class="img-pgm">
-                    <picture >
-                        <source class="lazy img-fluid" srcset="https://canalonce.mx/REST/data/miniaturas/pesadillas-financieras-tv-320x180.jpg" type="image/webp">
-                        <source class="lazy img-fluid" srcset="https://canalonce.mx/REST/data/miniaturas/pesadillas-financieras-tv-320x180.jpg" type="image/png"> 
-                        <img class="lazy img-fluid" src="https://canalonce.mx/REST/data/miniaturas/pesadillas-financieras-tv-320x180.jpg" alt="Logo Once" loading="lazy" title="Logo Once" width="88" height="61">
-                    </picture>
-                </div>
-                <div class="info-pgm">
-                    <h2>Titulo</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti molestiae, quis eum autem ipsum facilis possimus obcaecati eligendi dolore corporis ab voluptas perferendis rem quia quasi! Officia consequuntur inventore ex.</p>
-                </div>
-            </a>
-            <a href="#" class="item-result d-flex">
-                <div class="img-pgm">
-                    <picture >
-                        <source class="lazy img-fluid" srcset="https://canalonce.mx/REST/data/miniaturas/pesadillas-financieras-tv-320x180.jpg" type="image/webp">
-                        <source class="lazy img-fluid" srcset="https://canalonce.mx/REST/data/miniaturas/pesadillas-financieras-tv-320x180.jpg" type="image/png"> 
-                        <img class="lazy img-fluid" src="https://canalonce.mx/REST/data/miniaturas/pesadillas-financieras-tv-320x180.jpg" alt="Logo Once" loading="lazy" title="Logo Once" width="88" height="61">
-                    </picture>
-                </div>
-                <div class="info-pgm">
-                    <h2>Titulo</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti molestiae, quis eum autem ipsum facilis possimus obcaecati eligendi dolore corporis ab voluptas perferendis rem quia quasi! Officia consequuntur inventore ex.</p>
-                </div>
-            </a>
-            <a href="#" class="item-result d-flex">
-                <div class="img-pgm">
-                    <picture >
-                        <source class="lazy img-fluid" srcset="https://canalonce.mx/REST/data/miniaturas/pesadillas-financieras-tv-320x180.jpg" type="image/webp">
-                        <source class="lazy img-fluid" srcset="https://canalonce.mx/REST/data/miniaturas/pesadillas-financieras-tv-320x180.jpg" type="image/png"> 
-                        <img class="lazy img-fluid" src="https://canalonce.mx/REST/data/miniaturas/pesadillas-financieras-tv-320x180.jpg" alt="Logo Once" loading="lazy" title="Logo Once" width="88" height="61">
-                    </picture>
-                </div>
-                <div class="info-pgm">
-                    <h2>Titulo</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti molestiae, quis eum autem ipsum facilis possimus obcaecati eligendi dolore corporis ab voluptas perferendis rem quia quasi! Officia consequuntur inventore ex.</p>
-                </div>
-            </a>
-            <a href="#" class="item-result d-flex">
-                <div class="img-pgm">
-                    <picture >
-                        <source class="lazy img-fluid" srcset="https://canalonce.mx/REST/data/miniaturas/pesadillas-financieras-tv-320x180.jpg" type="image/webp">
-                        <source class="lazy img-fluid" srcset="https://canalonce.mx/REST/data/miniaturas/pesadillas-financieras-tv-320x180.jpg" type="image/png"> 
-                        <img class="lazy img-fluid" src="https://canalonce.mx/REST/data/miniaturas/pesadillas-financieras-tv-320x180.jpg" alt="Logo Once" loading="lazy" title="Logo Once" width="320" height="180">
-                    </picture>
-                </div>
-                <div class="info-pgm">
-                    <h2>Titulo</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti molestiae, quis eum autem ipsum facilis possimus obcaecati eligendi dolore corporis ab voluptas perferendis rem quia quasi! Officia consequuntur inventore ex.</p>
-                </div>
-            </a> -->
-
         </div>
     </article>
 </section>

59497d2a2cea77cec9d90953a24d98f9cdb8c90e
AlanAvila97 <fidelavila21@gmail.com>
2024-12-23 11:19:15 -0600

    funcionamiento y vistas de infomedia

--- Library/Layout/infomedia.php
+++ Library/Layout/infomedia.php
@@ -17,6 +17,12 @@
     <meta name="twitter:site" content="">
     <!-- Elements Header -->
 	<?php require_once("./Library/Layout/components/header_elements.php");?>
+    <!--  -->
+    <link rel="stylesheet" href="./assets/css/infomedia.css" media="none" onload="if(media!='all')media='all'"/>
+    <noscript><link rel="stylesheet" href="./assets/css/infomedia.css"></noscript>
+    <!--  -->
+    <link rel="preload" href="./assets/css/swiper/swiper-bundle.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript><link rel="stylesheet" href="./assets/css/swiper/swiper-bundle.min.css"></noscript>
 <body class="infomedia">
     <!-- Preloader -->
 	<?php require_once("./Library/Layout/components/preloader.php");?>
@@ -24,13 +30,18 @@
     <!-- Navbar -->
 	<?php require_once("./Library/Layout/components/nabvar.php");?>
     <!-- End Navbar -->
-    <!-- View -->
-	<?php require_once("./Library/Layout/views/infomedia_view.php");?>
-    <!-- End View -->
+    <main id="wrapper">
+        <!-- View -->
+        <?php require_once("./Library/Layout/views/infomedia_view.php");?>
+        <!-- End View -->
+    </main>
     <!-- Footer  -->
 	<?php require_once("./Library/Layout/components/footer.php");?>
     <!-- End Footer  -->     
     <!--  Footer Elements  -->
     <?php require_once("./Library/Layout/components/footer_elements.php");?>
     <!-- End Footer Elements-->
+    <script defer src="./assets/js/swiper/swiper-bundle.min.js"></script>
+    <!--  -->
+    <script defer src="./assets/js/infomedia.js"></script>
 </body>
---
+++ Library/Layout/views/infomedia_view.php
@@ -0,0 +1,67 @@
+<section class="section-infomedia pt-md-100">
+    <article class="container-infomedia">
+        <div class="sinopsis-infomedia d-flex">
+            <div class="info">
+                <h2>Infomedia</h2>
+                <p>
+                    En esta sección se ofrecen aclaraciones y correcciones de noticias,
+                    rumores y desinformación que circulan en medios de comunicación,
+                    redes sociales y otras plataformas.
+                    InfodemiaMx es una iniciativa del @SPRMexico
+                </p>
+            </div>
+        </div>
+    </article>
+</section>
+<section class="section-special-programs section-desmentidos-pgm">
+    <article class="container-special-programs">
+        <div class="title-special-programs desmentidos-pgm">
+            <h2>Desmentidos</h2>
+        </div> 
+        <div class="slider-special-programs d-flex">
+            <div class="content-navigation-prev c-pointer d-flex">
+                <img class="nav-prev" src="./assets/images/left-arrow.png" alt="" srcset="">
+            </div>
+            <div class="swiper swiper-desmentidos-pgm">
+                <div class="swiper-wrapper">
+                </div>
+            </div>
+            <div class="content-navigation-next c-pointer d-flex">
+                <img class="nav-next" src="./assets/images/right-arrow.png" alt="" srcset="">
+            </div>
+        </div>
+    </article>
+</section>
+<section class="section-info-social-networks">
+    <article class="container-info-social-networks">
+        <div class="title-info-social-networks">
+            <h2>
+                Desinformación y manipulación
+                en redes sociales
+            </h2>
+        </div> 
+        <div class="info-social-networks d-grid">
+            <a href="#" class="items-social-networks d-flex">
+                <picture >
+                    <source class="lazy img-fluid" srcset="./assets/images/info_re.png" type="image/webp">
+                    <source class="lazy img-fluid" srcset="./assets/images/info_re.png" type="image/png"> 
+                    <img class="lazy img-fluid" src="./assets/images/info_re.png" alt="Logo Mañanera" loading="lazy" title="Logo Mañanera" width="300" height="300">
+                </picture>  
+                <h2>
+                    Palabras relevantes
+                    del cuerpo de publicaciones
+                </h2>
+            </a>
+            <a href="#" class="items-social-networks d-flex">
+                <picture >
+                    <source class="lazy img-fluid" srcset="./assets/images/info_re_1.png" type="image/webp">
+                    <source class="lazy img-fluid" srcset="./assets/images/info_re_1.png" type="image/png"> 
+                    <img class="lazy img-fluid" src="./assets/images/info_re_1.png" alt="Logo Mañanera" loading="lazy" title="Logo Mañanera" width="300" height="300">
+                </picture>  
+                <h2>
+                    Proporción de publicaciones por tipo 
+                </h2>
+            </a>
+        </div>
+    </article>
+</section>
